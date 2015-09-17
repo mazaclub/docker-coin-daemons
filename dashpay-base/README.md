0ef0bcab70aa98e96842e6f7536e61851b534938
Rob Nelson <guruvan@maza.club>
2015-09-17 19:16:36 +0000

    Iinitial commit GRS

--- dashpay-base/README.md
+++
@@ -1,2 +0,0 @@
-# docker-dashd-base
-dashd dockerimage - automated build
--- dashpay-base/run-dashd-docker.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-  --name nmc_daemon \
-  -e TXINDEX=1 \
-  -e RPCPORT=9998 \
-  -e P2PPORT=9999 \
-  -v /opt/BIGDISK/NMC/home/coin:/home/coin \
-  mazaclub/dashd-base
