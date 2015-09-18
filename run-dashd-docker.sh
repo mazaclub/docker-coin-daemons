c9ef80ab8bcad3bd2e71c1ae3915943403d7fbd0
Rob Nelson <guruvan@maza.club>
2015-09-18 00:35:55 +0000

    Remove old readme, better caching  in Dockerfile

--- run-dashd-docker.sh
+++ run-dashd-docker.sh
@@ -1,7 +1,7 @@
 docker run -d \
-  --name nmc_daemon \
+  --name dash_daemon \
   -e TXINDEX=1 \
   -e RPCPORT=9998 \
   -e P2PPORT=9999 \
-  -v /opt/BIGDISK/NMC/home/coin:/home/coin \
+  -v /opt/BIGDISK/DASH/home/coin:/home/coin \
   mazaclub/dashd-base
